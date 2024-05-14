5ecbe1c69c5736bddebcfb39f85dee18b903a9e3
Vupv <vupv38@gmail.com>
2024-05-14 20:18:48 +0700

    rule

--- trello/.eslintrc.cjs
+++ trello/.eslintrc.cjs
@@ -21,7 +21,12 @@ module.exports = {
     'react-hooks/exhaustive-deps': 'warn',
     'react/prop-types': 0,
     'react/display-name': 0,
-
+    'no-restricted-imports': [
+      'error',
+      {
+        'patterns': ['@mui/*/*/*']
+      }
+    ],
     'no-console': 1,
     'no-lonely-if': 1,
     'no-unused-vars': 1,
