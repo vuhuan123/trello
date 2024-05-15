f9f7a3390f969edf0ebb27f6b95da1f768b2004f
Vupv <vupv38@gmail.com>
2024-05-15 16:12:53 +0700

    dark and light mode

--- trello/src/App.jsx
+++ trello/src/App.jsx
@@ -1,14 +1,28 @@
 import Button from '@mui/material/Button';
-import { Typography } from '@mui/material';
+import {
+  Experimental_CssVarsProvider as CssVarsProvider,
+  experimental_extendTheme as extendTheme,
+  useColorScheme,
+} from '@mui/material/styles';
+
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme();
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light');
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  );
+}
 
 function App() {
-
-
   return (
-
     <>
-
-
+      <ModeToggle />
       <Button variant="text" color='secondary'>Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
--- trello/src/theme.js
+++ trello/src/theme.js
@@ -1,24 +1,25 @@
-import { createTheme } from '@mui/material/styles';
-import { red } from '@mui/material/colors';
+
+import { cyan, deepOrange, orange, teal } from '@mui/material/colors';
+import { experimental_extendTheme as extendTheme } from '@mui/material/styles';
 
 // Create a theme instance.
-const theme = createTheme({
-  palette: {
-    mode: 'light',
-    primary: {
-      main: '#556cd6',
-    },
-    secondary: {
-      main: '#19857b',
-    },
-    error: {
-      main: red.A400,
-    },
-    text: {
+const theme = extendTheme({
+  colorSchemes: {
+    light: {
+      palette: {
+        primary: teal,
+        secondary: deepOrange,
 
-
+      },
+    },
+    dark: {
+      palette: {
+        primary: cyan,
+        secondary: orange,
+      },
     },
   },
+  // ...other properties
 });
 
 export default theme;
